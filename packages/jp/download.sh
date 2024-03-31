be72c7cfdc16046cef8c38fa6fd8fbc345ffd487
Ryan Parman <ryan@ryanparman.com>
2024-03-31 15:05:58 -0600

    Adding jp.

---
+++ packages/jp/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64)
+download-asset get \
+    --owner-repo "jmespath-community/jp" \
+    --pattern "jp-linux-amd64" \
+    --archive-path "" \
+    --write-to-bin "jq_amd64" \
+    ;
+
+# linux/arm64 (ARM64)
+download-asset get \
+    --owner-repo "jmespath-community/jp" \
+    --pattern "jp-linux-arm64" \
+    --archive-path "" \
+    --write-to-bin "jq_arm64" \
+    ;
