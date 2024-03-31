1b1b92219ddd2715a789dd42b584e9548aad4c1a
Ryan Parman <ryan@ryanparman.com>
2024-03-31 16:50:48 -0600

    Added pandoc.

---
+++ packages/pandoc/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64)
+download-asset get \
+    --owner-repo "jgm/pandoc" \
+    --pattern "pandoc-{{.Ver}}-linux-amd64.{{.Ext}}" \
+    --archive-path "pandoc" \
+    --write-to-bin "pandoc_amd64" \
+    ;
+
+# linux/arm64 (ARM64)
+download-asset get \
+    --owner-repo "jgm/pandoc" \
+    --pattern "pandoc-{{.Ver}}-linux-arm64.{{.Ext}}" \
+    --archive-path "pandoc" \
+    --write-to-bin "pandoc_arm64" \
+    ;
