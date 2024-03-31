760adc0a6a9f138d0c3fdd17ebe9b63efd4a5fac
Ryan Parman <ryan@ryanparman.com>
2024-03-31 16:59:12 -0600

    Added shellcheck.

---
+++ packages/shellcheck/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64)
+download-asset get \
+    --owner-repo "koalaman/shellcheck" \
+    --pattern "shellcheck-v{{.Ver}}.linux.x86_64.{{.Ext}}" \
+    --archive-path "shellcheck" \
+    --write-to-bin "shellcheck_amd64" \
+    ;
+
+# linux/arm64 (ARM64)
+download-asset get \
+    --owner-repo "koalaman/shellcheck" \
+    --pattern "shellcheck-v{{.Ver}}.linux.aarch64.{{.Ext}}" \
+    --archive-path "shellcheck" \
+    --write-to-bin "shellcheck_arm64" \
+    ;
