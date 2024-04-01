a7364c81332469af877adee5e63e72cd11718082
Ryan Parman <ryan@ryanparman.com>
2024-03-31 19:00:58 -0600

    Added GoReleaser OSS + Pro.

--- packages/goreleaser-pro/download.sh
+++ packages/goreleaser-pro/download.sh
@@ -6,7 +6,7 @@ download-asset get \
     --owner-repo "goreleaser/goreleaser-pro" \
     --pattern "goreleaser-pro_Linux_x86_64.{{.Ext}}" \
     --archive-path "goreleaser" \
-    --write-to-bin "goreleaser_amd64" \
+    --write-to-bin "goreleaser-pro_amd64" \
     ;
 
 # linux/arm64 (ARM64)
@@ -14,5 +14,5 @@ download-asset get \
     --owner-repo "goreleaser/goreleaser-pro" \
     --pattern "goreleaser-pro_Linux_arm64.{{.Ext}}" \
     --archive-path "goreleaser" \
-    --write-to-bin "goreleaser_arm64" \
+    --write-to-bin "goreleaser-pro_arm64" \
     ;
