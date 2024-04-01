77e208c52d3dffdc3e7ecf81cf454d209b9a04e4
Ryan Parman <ryan@ryanparman.com>
2024-04-01 10:54:21 -0600

    Added git-cliff.

---
+++ packages/git-cliff/download.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64) glibc
+download-asset get \
+    --owner-repo "orhun/git-cliff" \
+    --pattern "git-cliff-{{.Ver}}-x86_64-unknown-linux-gnu.{{.Ext}}" \
+    --archive-path "git-cliff" \
+    --write-to-bin "git-cliff_amd64" \
+    ;
+
+# linux/arm64 (ARM64) glibc
+download-asset get \
+    --owner-repo "orhun/git-cliff" \
+    --pattern "git-cliff-{{.Ver}}-aarch64-unknown-linux-gnu.{{.Ext}}" \
+    --archive-path "git-cliff" \
+    --write-to-bin "git-cliff_arm64" \
+    ;
+
+# linux/amd64 (Intel64) musl
+download-asset get \
+    --owner-repo "orhun/git-cliff" \
+    --pattern "git-cliff-{{.Ver}}-x86_64-unknown-linux-musl.{{.Ext}}" \
+    --archive-path "git-cliff" \
+    --write-to-bin "git-cliff_musl_amd64" \
+    ;
+
+# linux/arm64 (ARM64) musl
+download-asset get \
+    --owner-repo "orhun/git-cliff" \
+    --pattern "git-cliff-{{.Ver}}-aarch64-unknown-linux-musl.{{.Ext}}" \
+    --archive-path "git-cliff" \
+    --write-to-bin "git-cliff_musl_arm64" \
+    ;
