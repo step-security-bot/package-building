dfe70b5cd910d690ca76ab4ef3303cb10984c762
Ryan Parman <ryan@ryanparman.com>
2024-04-01 09:56:20 -0600

    Added golines.

---
+++ packages/golines/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64)
+download-asset get \
+    --owner-repo "segmentio/golines" \
+    --pattern "golines_{{.Ver}}_linux_amd64.{{.Ext}}" \
+    --archive-path "golines" \
+    --write-to-bin "golines_amd64" \
+    ;
+
+# linux/arm64 (ARM64)
+download-asset get \
+    --owner-repo "segmentio/golines" \
+    --pattern "golines_{{.Ver}}_linux_arm64.{{.Ext}}" \
+    --archive-path "golines" \
+    --write-to-bin "golines_arm64" \
+    ;
