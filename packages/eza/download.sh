bff92fd63a0d83b6dee6f03f6bdc1b10b019234c
Ryan Parman <ryan@ryanparman.com>
2024-04-02 14:07:13 -0600

    Added eza.

---
+++ packages/eza/download.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# linux/amd64 (Intel64) glibc
+download-asset get \
+    --owner-repo "sharkdp/fd" \
+    --pattern "fd-v{{.Ver}}-x86_64-unknown-linux-gnu.{{.Ext}}" \
+    --archive-path "fd-v{{.Ver}}-x86_64-unknown-linux-gnu/fd" \
+    --write-to-bin "fd_glibc_amd64" \
+    ;
+
+# linux/arm64 (ARM64) glibc
+download-asset get \
+    --owner-repo "sharkdp/fd" \
+    --pattern "fd-v{{.Ver}}-aarch64-unknown-linux-gnu.{{.Ext}}" \
+    --archive-path "fd-v{{.Ver}}-aarch64-unknown-linux-gnu/fd" \
+    --write-to-bin "fd_glibc_arm64" \
+    ;
