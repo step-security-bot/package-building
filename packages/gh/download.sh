6ca321f2a82b62f431250b0d08bbf4efccfc2d19
Ryan Parman <ryan@ryanparman.com>
2024-04-02 09:32:18 -0600

    Added gh cli.

--- packages/gh/download.sh
+++ packages/gh/download.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 # linux/amd64 (Intel64)
 download-asset get \
     --owner-repo "cli/cli" \
-    --pattern "GitHub CLI {{.Ver}} linux amd64" \
-    --archive-path "" \
+    --pattern "gh_{{.Ver}}_linux_amd64.{{.Ext}}" \
+    --archive-path "gh_{{.Ver}}_linux_amd64/bin/gh" \
     --write-to-bin "gh_amd64" \
     ;
 
 # linux/arm64 (ARM64)
 download-asset get \
     --owner-repo "cli/cli" \
-    --pattern "GitHub CLI {{.Ver}} linux arm64" \
-    --archive-path "" \
+    --pattern "gh_{{.Ver}}_linux_arm64.{{.Ext}}" \
+    --archive-path "gh_{{.Ver}}_linux_arm64/bin/gh" \
     --write-to-bin "gh_arm64" \
     ;
