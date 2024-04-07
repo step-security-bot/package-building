1088cceae57bf06deae13b31905bbf9c0ab10de3
Ryan Parman <ryan@ryanparman.com>
2024-04-07 13:53:08 -0600

    Attempt a musl-aarch64 build of lychee

--- packages/lychee/compile-alpine-arm64.sh
+++ packages/lychee/compile-alpine-arm64.sh
@@ -5,5 +5,7 @@ set -euo pipefail
 # _build-musl-*.yml always passes the package version as the first argument.
 PKG_VER="${1}"
 
+SAME_DIR="$(realpath "${BASH_SOURCE[0]%/*}")"
+
 # shellcheck disable=SC1091
-source ./compile-alpine.sh "${PKG_VER}" "arm64"
+source "${SAME_DIR}/compile-alpine.sh" "${PKG_VER}" "arm64"
