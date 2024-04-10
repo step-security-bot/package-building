92a204a9d99171f8e6b1dd9d62ec35036e255d9d
Ryan Parman <ryan@ryanparman.com>
2024-04-10 12:20:24 -0600

    Added gommit.

--- packages/gommit/download.sh
+++ packages/gommit/download.sh
@@ -10,7 +10,7 @@ git clone "${CLONE_REPO}" --branch "v${PKG_VER}" --single-branch /tmp/__BUILD__
 cd /tmp/__BUILD__
 
 # linux/amd64 (Intel64)
-GOARCH=amd64 go build -ldflags "-w -s" -trimpath -x -o /usr/local/bin/gommit_amd64 .
+GOARCH=amd64 go build -ldflags "-w -s" -trimpath -o /usr/local/bin/gommit_amd64 .
 
 # linux/arm64 (ARM64)
-GOARCH=arm64 go build -ldflags "-w -s" -trimpath -x -o /usr/local/bin/gommit_arm64 .
+GOARCH=arm64 go build -ldflags "-w -s" -trimpath -o /usr/local/bin/gommit_arm64 .
