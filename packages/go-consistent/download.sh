287314b021a09bcb54c80ed24499e318003c4a20
Ryan Parman <ryan@ryanparman.com>
2024-04-10 20:40:00 -0600

    Added go-consistent.

---
+++ packages/go-consistent/download.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "================================================================================"
+echo "Clone repo: ${CLONE_REPO} @ ${PKG_VER} to /tmp/__BUILD__"
+echo "================================================================================"
+
+# shellcheck disable=2154
+git clone "${CLONE_REPO}" --branch "v${PKG_VER}" --single-branch /tmp/__BUILD__
+cd /tmp/__BUILD__
+
+# linux/amd64 (Intel64)
+GOARCH=amd64 go build -ldflags "-w -s" -trimpath -o /usr/local/bin/go-consistent_amd64 .
+
+# linux/arm64 (ARM64)
+GOARCH=arm64 go build -ldflags "-w -s" -trimpath -o /usr/local/bin/go-consistent_arm64 .
