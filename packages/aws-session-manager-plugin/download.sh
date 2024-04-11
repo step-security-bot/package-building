3879c21f7dc0af70f8d110741ae358f03d329984
Ryan Parman <ryan@ryanparman.com>
2024-04-11 17:27:34 -0600

    Added aws-session-manager-plugin.

--- packages/aws-session-manager-plugin/download.sh
+++ packages/aws-session-manager-plugin/download.sh
@@ -9,10 +9,14 @@ echo "==========================================================================
 git clone "${CLONE_REPO}" --branch "${PKG_VER}.0" --single-branch /tmp/__BUILD__
 cd /tmp/__BUILD__
 
+go mod init session-manager-plugin
+go mod tidy
+go get github.com/twinj/uuid@990eabe
+
 # linux/amd64 (Intel64)
 # https://github.com/aws/session-manager-plugin/blob/e12e3d7a44af6321f3c12d156df7f60b596f6628/makefile#L87-L88
-GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o /usr/local/bin/session-manager-plugin_amd64 -v src/sessionmanagerplugin-main/main.go
+GOARCH=amd64 go build -mod=readonly -ldflags "-s -w" -o /usr/local/bin/session-manager-plugin_amd64 -v src/sessionmanagerplugin-main/main.go
 
 # linux/arm64 (ARM64)
 # https://github.com/aws/session-manager-plugin/blob/e12e3d7a44af6321f3c12d156df7f60b596f6628/makefile#L109-L110
-GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags=-Wl,-z,now,-z,relro,-z,defs" -o /usr/local/bin/session-manager-plugin_arm64 -v src/sessionmanagerplugin-main/main.go
+GOARCH=arm64 go build -mod=readonly -ldflags "-s -w -extldflags=-Wl,-z,now,-z,relro,-z,defs" -o /usr/local/bin/session-manager-plugin_arm64 -v src/sessionmanagerplugin-main/main.go
