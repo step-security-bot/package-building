cb212a26c3fde84b133f1bbe6689c6600dab5db9
Ryan Parman <ryan@ryanparman.com>
2024-04-12 13:34:53 -0600

    Added amazon-cloudwatch-agent.

--- packages/amazon-cloudwatch-agent/download.sh
+++ packages/amazon-cloudwatch-agent/download.sh
@@ -6,20 +6,20 @@ mkdir -p "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist"
 
 # linux/amd64 (Intel64) RPM
 # shellcheck disable=2154
-wget "https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/${PKG_VER}.0/linux_amd64/amazon-ssm-agent.rpm" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/${PKG_VER}/amazon-cloudwatch-agent.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.x86_64.rpm"
 
 # linux/arm64 (ARM64) RPM
 # shellcheck disable=2154
-wget "https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/${PKG_VER}.0/linux_arm64/amazon-ssm-agent.rpm" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/${PKG_VER}/amazon-cloudwatch-agent.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.aarch64.rpm"
 
 # linux/amd64 (Intel64) DEB
 # shellcheck disable=2154
-wget "https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/${PKG_VER}.0/debian_amd64/amazon-ssm-agent.deb" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/${PKG_VER}/amazon-cloudwatch-agent.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_amd64.deb"
 
 # linux/arm64 (ARM64) DEB
 # shellcheck disable=2154
-wget "https://s3.us-east-2.amazonaws.com/amazon-ssm-us-east-2/${PKG_VER}.0/debian_arm64/amazon-ssm-agent.deb" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/${PKG_VER}/amazon-cloudwatch-agent.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_arm64.deb"
