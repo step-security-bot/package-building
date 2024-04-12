bbf8001bc6d9ce618b741940167bef9ef47b1fea
Ryan Parman <ryan@ryanparman.com>
2024-04-12 12:03:43 -0600

    Fixing aws-session-manager-plugin.

--- packages/aws-session-manager-plugin/download.sh
+++ packages/aws-session-manager-plugin/download.sh
@@ -6,20 +6,20 @@ mkdir -p "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist"
 
 # linux/amd64 (Intel64) RPM
 # shellcheck disable=2154
-wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}/linux_64bit/session-manager-plugin.rpm" \
+wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}.0/linux_64bit/session-manager-plugin.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.x86_64.rpm"
 
 # linux/arm64 (ARM64) RPM
 # shellcheck disable=2154
-wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}/linux_arm64/session-manager-plugin.rpm" \
+wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}.0/linux_arm64/session-manager-plugin.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.aarch64.rpm"
 
 # linux/amd64 (Intel64) DEB
 # shellcheck disable=2154
-wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}/ubuntu_64bit/session-manager-plugin.deb" \
+wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}.0/ubuntu_64bit/session-manager-plugin.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_amd64.deb"
 
 # linux/arm64 (ARM64) DEB
 # shellcheck disable=2154
-wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}/ubuntu_arm64/session-manager-plugin.deb" \
+wget "https://s3.amazonaws.com/session-manager-downloads/plugin/${PKG_VER}.0/ubuntu_arm64/session-manager-plugin.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_arm64.deb"
