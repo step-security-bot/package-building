1f81912477938d3676707d0fce961f7a51979547
Ryan Parman <ryan@ryanparman.com>
2024-04-12 13:37:30 -0600

    Added amazon-cloudwatch-agent.

--- packages/amazon-cloudwatch-agent/download.sh
+++ packages/amazon-cloudwatch-agent/download.sh
@@ -6,20 +6,20 @@ mkdir -p "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist"
 
 # linux/amd64 (Intel64) RPM
 # shellcheck disable=2154
-wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/${PKG_VER}/amazon-cloudwatch-agent.rpm" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/v${PKG_VER}/amazon-cloudwatch-agent.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.x86_64.rpm"
 
 # linux/arm64 (ARM64) RPM
 # shellcheck disable=2154
-wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/${PKG_VER}/amazon-cloudwatch-agent.rpm" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/v${PKG_VER}/amazon-cloudwatch-agent.rpm" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}-${PKG_VER}-1.aarch64.rpm"
 
 # linux/amd64 (Intel64) DEB
 # shellcheck disable=2154
-wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/${PKG_VER}/amazon-cloudwatch-agent.deb" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/amd64/v${PKG_VER}/amazon-cloudwatch-agent.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_amd64.deb"
 
 # linux/arm64 (ARM64) DEB
 # shellcheck disable=2154
-wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/${PKG_VER}/amazon-cloudwatch-agent.deb" \
+wget "https://amazoncloudwatch-agent-us-east-2.s3.us-east-2.amazonaws.com/amazon_linux/arm64/v${PKG_VER}/amazon-cloudwatch-agent.deb" \
     --output-document "${GITHUB_WORKSPACE}/packages/${PKG_NAME}/dist/${PKG_NAME}_${PKG_VER}-1_arm64.deb"
