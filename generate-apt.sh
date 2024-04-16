559da8515c5fcd07667fda0902320eef5eba262f
Ryan Parman <ryan@ryanparman.com>
2024-04-16 10:31:11 -0600

    Fix apt Release file format.

--- generate-apt.sh
+++ generate-apt.sh
@@ -5,7 +5,6 @@ PATH_TO_READ="${1}"
 
 cat <<EOF
 Origin: Northwood Labs Repository
-Label: Northwood Labs Repository
 Suite: stable
 Version: 1.0
 Codename: stable
@@ -18,7 +17,7 @@ EOF
 echo "MD5Sum:"
 # shellcheck disable=2016
 find "${PATH_TO_READ}" -type f -name "Packages*" -print0 | xargs -0 -I% bash -c '
-    echo " $(md5sum "%" | cut --delimiter=" " --fields=1) $(wc --bytes "%" | sed -r "s,.*dists/stable/,,")"
+    echo " $(md5sum "%" | cut --delimiter=" " --fields=1) $(wc --bytes "%" | sed -r "s,dists/stable/,,")"
 '
 echo "SHA1:"
 # shellcheck disable=2016
